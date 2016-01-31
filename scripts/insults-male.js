e6cd40231036f854c5940b4bfba8e18c4ae7df6f
nw_thecodingwizard <nathan.r.wang@gmail.com>
2016-01-31 23:13:41 +0000

    Added 4 insults to both genders

--- scripts/insults-male.js
+++ scripts/insults-male.js
@@ -4,9 +4,15 @@ if (!("insults" in window)) {
 
 window.insults.male = [
     {
-        "message": "You suck"
+        "message": "You must have been born on a highway, because that&apos;s where most accidents happen."
     },
     {
-        "message": "Hi suck"
+        "message": "Maybe when you&apos;re 100 years old you might figure out that 1 + 1 is 2."
+    },
+    {
+        "message": "It looks like your face caught on fire and someone tried to put it out with a fork."
+    },
+    {
+        "message": "I would ask how old you are, but I know you can&apos;t count that high."
     }
 ];
